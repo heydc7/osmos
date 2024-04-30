6eb326ff2d9bae8712b84009d994cafbfc77ce49
heydc7 <dc6707914@gmail.com>
2024-04-30 01:58:42 -0400

    Optimized GUI

--- cmd/root.go
+++ cmd/root.go
@@ -4,112 +4,208 @@ Copyright © 2024 OSMOS
 package cmd
 
 import (
+	"crypto/tls"
 	"fmt"
-	"io"
-	"math/rand"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/spf13/cobra"
 )
 
-func checkParameterReflection(inputURL string) {
-	// Parse the URL
-	parsedURL, err := url.Parse(inputURL)
+type paramCheck struct {
+	url   string
+	param string
+}
+
+var transport = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: time.Second,
+		DualStack: true,
+	}).DialContext,
+}
+
+var httpClient = &http.Client{
+	Transport: transport,
+}
+
+func checkReflected(targetURL string) ([]string, error) {
+
+	out := make([]string, 0)
+
+	req, err := http.NewRequest("GET", targetURL, nil)
 	if err != nil {
-		fmt.Println("Error parsing URL:", err)
-		return
+		return out, err
 	}
 
-	// Extract host and parameters
-	host := parsedURL.Host
-	parameters := parsedURL.Query()
+	req.Header.Add("User-Agent", "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
 
-	if host != "127.0.0.1" || host != "localhost" {
-		fmt.Println("Invalid Host", err)
-		return
-	}
-
-	// Send HTTP request
-	resp, err := http.Get(inputURL)
+	resp, err := httpClient.Do(req)
 	if err != nil {
-		fmt.Println("Error making HTTP request:", err)
-		return
+		return out, err
+	}
+	if resp.Body == nil {
+		return out, err
 	}
 	defer resp.Body.Close()
 
-	// Read the response body
-	body, err := io.ReadAll(resp.Body)
+	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-	//fmt.Println(resp.Body)
-	// Check if any parameter value is reflected in the response
-	for key, values := range parameters {
-		for _, value := range values {
-			if strings.Contains(string(body), value) {
-				fmt.Printf("Parameter value %s is reflected in the response for parameter <%s>\n", value, key)
-				fuzzParameter(key, value, inputURL)
-			} else {
-				fmt.Printf("Parameter value %s is not reflected in the response for parameter <%s>\n", value, key)
-			}
-		}
-	}
-}
-
-func fuzzParameter(parameterKey, originalValue, inputURL string) {
-	// Character sets
-	characterSets := map[string]string{
-		"Alphabets":   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		"Numbers":     "0123456789",
-		"Punctuation": "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+		return out, err
 	}
 
-	// Seed the random number generator
-	rand.Seed(time.Now().UnixNano())
+	if strings.HasPrefix(resp.Status, "3") {
+		return out, nil
+	}
 
-	// Fuzz the parameter with random sets of random lengths
-	for setName, charSet := range characterSets {
-		fmt.Printf("Fuzzing parameter %s with %s:\n", parameterKey, setName)
-		for i := 0; i < 5; i++ { // fuzz with 5 random sets
-			fuzzedValue := generateRandomSet(charSet)
-			// Construct a new URL with the fuzzed parameter
-			fuzzedURL := strings.Replace(inputURL, originalValue, fuzzedValue, -1)
-			// Send a request with the fuzzed URL
-			resp, err := http.Get(fuzzedURL)
-			if err != nil {
-				fmt.Println("Error making HTTP request:", err)
+	ct := resp.Header.Get("Content-Type")
+	if ct != "" && !strings.Contains(ct, "html") {
+		return out, nil
+	}
+
+	body := string(b)
+
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return out, err
+	}
+
+	for key, vv := range u.Query() {
+		for _, v := range vv {
+			if !strings.Contains(body, v) {
 				continue
 			}
-			defer resp.Body.Close()
-			// Check if the fuzzed value is reflected
-			var body strings.Builder
-			_, err = io.Copy(&body, resp.Body)
-			if err != nil {
-				fmt.Println("Error reading response body:", err)
-				continue
-			}
-			if strings.Contains(body.String(), fuzzedValue) {
-				fmt.Printf("Fuzzed value %s is reflected in the response for parameter <%s>\n", fuzzedValue, parameterKey)
-			} else {
-				fmt.Printf("Fuzzed value %s is not reflected in the response for parameter <%s>\n", fuzzedValue, parameterKey)
-			}
+
+			out = append(out, key)
 		}
 	}
+
+	return out, nil
 }
 
-func generateRandomSet(charSet string) string {
-	length := rand.Intn(5) + 1 // random length between 1 and 5
-	var sb strings.Builder
-	for i := 0; i < length; i++ {
-		randomIndex := rand.Intn(len(charSet))
-		sb.WriteByte(charSet[randomIndex])
+func checkAppend(targetURL, param, payload string) (bool, error) {
+	
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return false, err
 	}
-	return sb.String()
+
+	qs := u.Query()
+	//val := qs.Get(param)
+	//if val == "" {
+	//return false, nil
+	//return false, fmt.Errorf("can't append to non-existant param %s", param)
+	//}
+
+	qs.Set(param, payload)
+	u.RawQuery = qs.Encode()
+	reflected, err := checkReflected(u.String())
+	if err != nil {
+		return false, err
+	}
+
+	for _, r := range reflected {
+		if r == param {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+type workerFunc func(paramCheck, chan paramCheck)
+
+func makePool(input chan paramCheck, fn workerFunc) chan paramCheck {
+	var wg sync.WaitGroup
+
+	output := make(chan paramCheck)
+	for i := 0; i < 40; i++ {
+		wg.Add(1)
+		go func() {
+			for c := range input {
+				fn(c, output)
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+
+	return output
+}
+
+func main(url string) {
+	httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	initialChecks := make(chan paramCheck, 40)
+
+	appendChecks := makePool(initialChecks, func(c paramCheck, output chan paramCheck) {
+		reflected, err := checkReflected(c.url)
+		if err != nil {
+			//fmt.Fprintf(os.Stderr, "error from checkReflected: %s\n", err)
+			return
+		}
+
+		if len(reflected) == 0 {
+			// TODO: wrap in verbose mode
+			fmt.Printf("[*] No Reflection Detected")
+			return
+		}
+
+		for _, param := range reflected {
+			fmt.Printf("[*] Reflected Parameter: %s\n", param)
+			fmt.Println("[*] Unfiltered: Alphabets")
+			fmt.Println("[*] Unfiltered: Numbers")
+			output <- paramCheck{c.url, param}
+		}
+	})
+
+	charChecks := makePool(appendChecks, func(c paramCheck, output chan paramCheck) {
+		wasReflected, err := checkAppend(c.url, c.param, "iy3j4h234hjb23234")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error from checkAppend for url %s with param %s: %s", c.url, c.param, err)
+			return
+		}
+
+		if wasReflected {
+			output <- paramCheck{c.url, c.param}
+		}
+	})
+
+	done := makePool(charChecks, func(c paramCheck, output chan paramCheck) {
+		output_of_url := []string{c.url, c.param}
+		for _, char := range []string{"\"", "'", "<", ">", "$", "|", "(", ")", "`", ":", ";", "{", "}"} {
+			wasReflected, err := checkAppend(c.url, c.param, "pfx"+char+"sfx")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error from checkAppend for url %s with param %s with %s: %s", c.url, c.param, char, err)
+				continue
+			}
+
+			if wasReflected {
+				output_of_url = append(output_of_url, char)
+			}
+		}
+		if len(output_of_url) >= 2 {
+			fmt.Printf("[*] Unfiltered: Special Chars %v \n", output_of_url[2:])
+		}
+	})
+
+	initialChecks <- paramCheck{url: url}
+
+	close(initialChecks)
+	<-done
 }
 
 var rootCmd = &cobra.Command{
@@ -132,7 +228,7 @@ Usage of osmos for fuzzing targets without prior mutual consent is illegal. It i
 
 		if url != "" {
 			fmt.Println("URL:", url)
-			checkParameterReflection(url)
+			main(url)
 		}
 		if request != "" {
 			fmt.Println("Request:", request)
